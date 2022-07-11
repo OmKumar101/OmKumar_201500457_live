a1e578a7e7480ff15778e6c13dcca9c91783e99e
OmKumar <omk99881122344@gmail.com>
2022-07-11 22:26:52 +0530

    initialcode

---
+++ 1.js
@@ -0,0 +1,7 @@
+document.getElementById("name").innerHTML="Om Kumar";
+document.getElementById("roll-no").innerHTML="201500457";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+
+var date=new Date();
+
+document.getElementById("date").innerHTML=date.toDateString();
